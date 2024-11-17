c98fd4478b1dd7cd6bf58d47ecd5e9f4136b4464
Jocy Neto - Linux <jocygneto@gmail.com>
2024-11-17 08:24:46 -0300

    Finalizando Auth e JSON Web Token (JWT) para implementacao do restante do projeto

--- projeto3/routers/auth.py
+++ projeto3/routers/auth.py
@@ -11,14 +11,15 @@ from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import JWTError, jwt
 
-router = APIRouter()
+# region generaly configs
+router = APIRouter(prefix="/auth", tags=["auth"])
 
 SECRETE_KEY = "7239c3788d35e461ca218721bad9186f40869f9f2a7cdf21a85229e88a1d2f27" # openssl rand -hex 32
 ALGORITHM = "HS256"
 
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 #Abre e fecha conexao
 def get_db():
@@ -31,7 +32,9 @@ def get_db():
 #Injecao de depencia que fala que é necessário fazer algo antes de executar o codigo
 #No caso depende da funcao get_db.
 db_dependecy = Annotated[Session, Depends(get_db)]
+#endregion
 
+# region Util Funcs 
 def autenticate_user(username: str, password:str, db):
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
@@ -60,9 +63,10 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
      except JWTError:
          raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail="Não foi possivel validar usuário.")
+#endregion
 
-
-@router.post("/auth/add", status_code=status.HTTP_201_CREATED)
+# region Requests Methods
+@router.post("/add", status_code=status.HTTP_201_CREATED)
 async def create_user(user_request: User_Model, 
                       db: db_dependecy):
     create_user_model = Users(
@@ -85,4 +89,5 @@ async def login_for_acess_token(form_data: Annotated[OAuth2PasswordRequestForm, 
     if not user:
         return "Failed Authentication"
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
-    return {"acess_token": token, "token_type": "Bearer"}
+    return {"acess_token": token, "token_type": "bearer"}
+#endregion
